9828eb02d72b6af56e96ded72a04f3f9d5573daf
kemalkaancetindag <kaancetindag@icloud.com>
2020-07-26 14:07:45 +0300

    tags page done

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const { slugify } = require("./src/util/utilityFunctions")
 const path = require("path")
 const { authors } = require("./src/util/authors")
+const _ = require("lodash")
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === "MarkdownRemark") {
@@ -15,7 +16,11 @@ exports.onCreateNode = ({ node, actions }) => {
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
-  const singlePostTemplate = path.resolve("src/templates/single-post.js")
+
+  const templates = {
+    singlePost: path.resolve("src/templates/single-post.js"),
+    tagsPage: path.resolve("src/templates/tags-page.js"),
+  }
 
   return graphql(`
     {
@@ -24,6 +29,7 @@ exports.createPages = ({ actions, graphql }) => {
           node {
             frontmatter {
               author
+              tags
             }
             fields {
               slug
@@ -40,7 +46,7 @@ exports.createPages = ({ actions, graphql }) => {
     posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: singlePostTemplate,
+        component: templates.singlePost,
         context: {
           slug: node.fields.slug,
           imageUrl: authors.find(
@@ -48,6 +54,29 @@ exports.createPages = ({ actions, graphql }) => {
           ).imageUrl,
         },
       })
+    })
+    let tags = []
+    _.each(posts, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    let tagPostCounts = {}
+
+    tags.forEach(tag => {
+      tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1
+    })
+
+    tags = _.uniq(tags)
+
+    createPage({
+      path: `/tags`,
+      component: templates.tagsPage,
+      context: {
+        tags,
+        tagPostCounts,
+      },
     })
   })
 }
